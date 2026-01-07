71f1e436d0fd5beb2781af68a37dc60a7aae9a68
MikolajSobecki <92922311+MikolajSobecki@users.noreply.github.com>
2026-01-07 19:32:04 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const words = [
-  { en: "apple", pl: "jabłko" },
-  { en: "house", pl: "dom" },
-  { en: "car", pl: "samochód" },
-  { en: "book", pl: "książka" },
-  { en: "water", pl: "woda" }
+  { en: "Being gay is easy.", pl: "Bycie gejem jest proste." },
+  { en: "Straight guys fear gay men the way mirrors fear honesty.", pl: "Hetero boją się gejów tak jak lustra boją się prawdy." },
+  { en: "I’m gay, horny, and full of bad ideas.", pl: "Jestem gejem, napalony i pełen złych pomysłów." },
+  { en: "I don’t need love, I need dick and peace.", pl: "Nie potrzebuję miłości, potrzebuję penisa i spokoju." },
+  { en: "My ass has better opinions than my brain.", pl: "Moja dupa ma lepsze opinie niż mój mózg." }
 ];
 
 let currentWord = null;
